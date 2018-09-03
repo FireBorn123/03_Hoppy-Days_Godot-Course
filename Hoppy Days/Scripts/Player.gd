7c5ccfa0578d38492b0c7a03287eb469701132bd
TREVOR-PC\Trevor <thesilverdrake@gmail.com>
2018-09-03 14:35:25 -0600

    Lecture 39: PhysicsBody2D –Making a Character

---
+++ Hoppy Days/Scripts/Player.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+const SPEED = 750
+
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+		motion.x = SPEED
+	elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+		motion.x = -SPEED
+	else:
+		motion.x = 0
+#$	if Input.is_action_just_pressed("ui_up"):
+	move_and_slide(motion)
