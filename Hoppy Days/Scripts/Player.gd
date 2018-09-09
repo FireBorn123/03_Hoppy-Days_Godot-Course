f4abb948db33a569d9ed31a9b7f3665c05e5ad50
TREVOR-PC\Trevor <thesilverdrake@gmail.com>
2018-09-08 18:26:52 -0600

    Lecture 40: AnimatedSprite – Making Bunny Run

--- Hoppy Days/Scripts/Player.gd
+++ Hoppy Days/Scripts/Player.gd
@@ -8,9 +8,15 @@ var motion = Vector2()
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
 		motion.x = SPEED
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("run")
 	elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
 		motion.x = -SPEED
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("run")
 	else:
 		motion.x = 0
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("idle")
 #$	if Input.is_action_just_pressed("ui_up"):
 	move_and_slide(motion)
