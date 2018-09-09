76037205f39e5a7c284ce549960284b47e423b50
TREVOR-PC\Trevor <thesilverdrake@gmail.com>
2018-09-09 15:09:26 -0600

    Lecture 41: Jumping Commit

--- Hoppy Days/Scripts/Player.gd
+++ Hoppy Days/Scripts/Player.gd
@@ -1,11 +1,25 @@
 extends KinematicBody2D
 
 const SPEED = 750
-
+const GRAVITY = 3000
+const UP = Vector2(0, -1)
+const JUMP_SPEED = -900
 
 var motion = Vector2()
 
 func _physics_process(delta):
+	fall(delta)
+	run()
+	jump()
+	move_and_slide(motion, UP)
+
+func fall(delta):
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y += GRAVITY * delta
+
+func run():
 	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
 		motion.x = SPEED
 		$AnimatedSprite.flip_h = false
@@ -18,5 +32,10 @@ func _physics_process(delta):
 		motion.x = 0
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("idle")
-#$	if Input.is_action_just_pressed("ui_up"):
-	move_and_slide(motion)
+
+
+func jump():
+	if is_on_floor() and Input.is_action_pressed("ui_up"):
+		motion.y = JUMP_SPEED
+
+	
