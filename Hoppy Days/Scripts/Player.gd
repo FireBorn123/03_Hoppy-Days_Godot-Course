fd90bfb27707404368803060d674c7f66c7f51e5
TREVOR-PC\Trevor <thesilverdrake@gmail.com>
2018-09-15 22:12:57 -0600

    Lecture 42: Cleaning Up The Player Code Commit

--- Hoppy Days/Scripts/Player.gd
+++ Hoppy Days/Scripts/Player.gd
@@ -3,35 +3,41 @@ extends KinematicBody2D
 const SPEED = 750
 const GRAVITY = 3000
 const UP = Vector2(0, -1)
-const JUMP_SPEED = -900
+const JUMP_SPEED = -1200
 
 var motion = Vector2()
 
 func _physics_process(delta):
+	update_motion(delta)
+
+func _process(delta):
+	update_animation(motion)
+
+func update_motion(delta):
 	fall(delta)
 	run()
 	jump()
 	move_and_slide(motion, UP)
 
+func update_animation(motion):
+	$AnimatedSprite.update(motion)
+
 func fall(delta):
 	if is_on_floor():
 		motion.y = 0
 	else:
-		motion.y += GRAVITY * delta
+		motion.y += GRAVITY * delta #Essentially because we have delta built into move and slide, we have m/s^2 (Gravity *delta*delta)
 
 func run():
 	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
 		motion.x = SPEED
-		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.play("run")
+
 	elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
 		motion.x = -SPEED
-		$AnimatedSprite.flip_h = true
-		$AnimatedSprite.play("run")
+
 	else:
 		motion.x = 0
-		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.play("idle")
+
 
 
 func jump():
---
+++ Hoppy Days/Scripts/PlayerAnimation.gd
@@ -0,0 +1,15 @@
+extends AnimatedSprite
+
+func update(motion):
+	if motion.y < 0:
+		play("jump")
+	elif motion.x > 0:
+		flip_h = false
+		play("run")
+	elif motion.x < 0:
+		flip_h = true
+		play("run")
+	else:
+		flip_h = false
+		play("idle")
+
